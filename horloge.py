52d0fe61e4780bcf79a2b6bf5fa31a287d2aabe0
haroutioun-togramadzhyan <145985803+haroutioun-togramadzhyan@users.noreply.github.com>
2024-01-08 10:24:40 +0100

    Delete horloge.py

--- horloge.py
+++
@@ -1,8 +0,0 @@
-import time
-
-def tom():
-    print(time.strftime("%H:%M:%S"))
-    t_am_pm = time.strftime('%I:%M:%S %p')
-    print("Format anglophone(AM/PM):", t_am_pm)
-    time.sleep(1)
-tom()
